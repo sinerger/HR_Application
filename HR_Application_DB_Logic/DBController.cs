e1851848ef404999212e70c8ce34b06a15862b04
Rostislav <skochmen@gmail.com>
2021-05-26 21:36:42 +0300

    Refactoring

--- HR_Application_DB_Logic/DBController.cs
+++ HR_Application_DB_Logic/DBController.cs
@@ -10,16 +10,15 @@ namespace HR_Application_DB_Logic
     public static class DBController
     {
         private static string _connectionString = @"Server=(LocalDB)\MSSQLLocalDB; DataBase = NewEmployeesDB; Trusted_Connection = True; Integrated Security = True;";
-        private static string _query = string.Empty;
 
         public static List<GeneralInformationDTO> GetGeneralInformationDTOByEmployeeID(int employeeID)
         {
-            _query = "exec GetGeneralInformationByEmployeeID @employeeID";
+            string query = "exec GetGeneralInformationByEmployeeID @employeeID";
             var result = new List<GeneralInformationDTO>();
 
             using (IDbConnection dbConnection = new SqlConnection(_connectionString))
             {
-                result = dbConnection.Query<GeneralInformationDTO>(_query, new { employeeID }).AsList<GeneralInformationDTO>();
+                result = dbConnection.Query<GeneralInformationDTO>(query, new { employeeID }).AsList<GeneralInformationDTO>();
             }
 
             return result;
@@ -27,13 +26,13 @@ namespace HR_Application_DB_Logic
 
         public static EmployeeDTO GetEmployeeDTOByID(int id)
         {
-            _query = "exec GetEmployeeByID @ID";
+            string query = "exec GetEmployeeByID @ID";
 
             var result = new EmployeeDTO();
 
             using (IDbConnection dbConnection = new SqlConnection(_connectionString))
             {
-                result = dbConnection.QuerySingle<EmployeeDTO>(_query, new { id });
+                result = dbConnection.QuerySingle<EmployeeDTO>(query, new { id });
             }
 
             return result;
