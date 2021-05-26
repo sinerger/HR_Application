cd3639a1413fdf625e6d9c194e4c5ebb84d3214c
Rostislav <skochmen@gmail.com>
2021-05-26 11:15:18 +0300

    Rebase folders

--- HR_Application_DB/Tables/Employees.sql
+++ HR_Application_DB/Tables/Employees.sql
@@ -1,28 +1,14 @@
-﻿CREATE TABLE [Employees] (
-	ID int NOT NULL IDENTITY,
-	Photo nvarchar(255),
-	FirstName nvarchar(255) NOT NULL,
-	LastName nvarchar(255) NOT NULL,
-	RegistationDate datetime NOT NULL,
-	StatusID int NOT NULL,
-	LocationID int NOT NULL,
-	IsActual bit NOT NULL,
-  CONSTRAINT [PK_EMPLOYEES] PRIMARY KEY CLUSTERED
-  (
-  [ID] ASC
-  ) WITH (IGNORE_DUP_KEY = OFF)
-
+﻿CREATE TABLE [dbo].[Employees] (
+    [ID]              INT            IDENTITY (1, 1) NOT NULL,
+    [Photo]           NVARCHAR (255) NULL,
+    [FirstName]       NVARCHAR (255) NOT NULL,
+    [LastName]        NVARCHAR (255) NOT NULL,
+    [RegistationDate] DATETIME       NOT NULL,
+    [StatusID]        INT            NOT NULL,
+    [LocationID]      INT            NOT NULL,
+    [IsActual]        BIT            NOT NULL,
+    CONSTRAINT [PK_EMPLOYEES] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [Employees_fk0] FOREIGN KEY ([LocationID]) REFERENCES [dbo].[Locations] ([ID]),
+    CONSTRAINT [Employees_fk1] FOREIGN KEY ([StatusID]) REFERENCES [dbo].[Statuses] ([ID])
 );
-GO
-ALTER TABLE [Employees] WITH CHECK ADD CONSTRAINT [Employees_fk0] FOREIGN KEY ([LocationID]) REFERENCES [Locations]([ID])
-
-GO
-ALTER TABLE [Employees] CHECK CONSTRAINT [Employees_fk0]
-GO
-GO
-ALTER TABLE [Employees] WITH CHECK ADD CONSTRAINT [Employees_fk1] FOREIGN KEY ([StatusID]) REFERENCES [Statuses]([ID])
-
-GO
-ALTER TABLE [Employees] CHECK CONSTRAINT [Employees_fk1]
-GO
 
