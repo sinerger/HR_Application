39f7ccd9c6687429d1b7c81e749c3e371cdcac2e
Serhii <avakyanms@gmail.com>
2021-05-27 16:22:15 +0300

    Added EmployeeAdressDTO and EmployeeProjectsWithDirectionDTO

---
+++ HR_Application_DB_Logic/Models/EmployeeProjectsWithDirection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR_Application_DB_Logic.Models
+{
+    public class EmployeeProjectsWithDirection
+    {
+        public int? ID { get; set; }
+        public string Photo { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string RegistationDate { get; set; }
+        public ProjectDTO Project { get; set; }
+        public DirectionDTO Direction { get; set; }
+        public bool IsActual { get; set; }
+    }
+}
