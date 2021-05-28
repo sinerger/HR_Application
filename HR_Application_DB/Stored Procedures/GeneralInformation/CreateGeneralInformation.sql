549802219ce7324e49ba7e2cbd014adad6994fdd
Rostislav <skochmen@gmail.com>
2021-05-28 12:37:55 +0300

    Create CRUD into GeneralInformationRepository

---
+++ HR_Application_DB/Stored Procedures/GeneralInformation/CreateGeneralInformation.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[CreateGeneralInformation]
+	@ID int ,
+	@EmployeeID int,
+	@Sex nvarchar,
+	@Education nvarchar,
+	@FamilyStatusID int,
+	@Phone nvarchar,
+	@Email nvarchar,
+	@BirthDate dateTime,
+	@Hobby nvarchar,
+	@AmountChildren int
+
+AS
+insert into [dbo].[GeneralInformation]
+values(
+	@ID,
+	@EmployeeID,
+	@Sex,
+	@Education,
+	@FamilyStatusID,
+	@Phone,
+	@Email,
+	@BirthDate,
+	@Hobby,
+	@AmountChildren)
