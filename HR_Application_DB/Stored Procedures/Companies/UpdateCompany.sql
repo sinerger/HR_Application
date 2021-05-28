1ae7993ff08de3ec84ad3131da295147625df991
Rostislav <skochmen@gmail.com>
2021-05-28 16:12:02 +0300

    Create CRUD into CompanyRepository

---
+++ HR_Application_DB/Stored Procedures/Companies/UpdateCompany.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[UpdateCompany]
+	@ID int,
+	@Title nvarchar,
+	@LocationID int,
+	@Description nvarchar
+AS
+	update [dbo].[Companies]
+	set
+	[dbo].[Companies].Title = @Title,
+	[dbo].[Companies].LocationID = @LocationID,
+	[dbo].[Companies].[Description] = @Description
+	
+	where [dbo].[Companies].ID = @ID
