31576cfaa9aca6be40d8e2956d81fadf02332266
Alexandr Shpiganovich <semben22@gmail.com>
2021-05-28 20:42:12 +0300

    Complete task

---
+++ HR_Application_DB/Stored Procedures/Locations/CreateLocation.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [dbo].[CreateLocation]
+	@CityID int,
+	@Street nvarchar,
+	@HouseNumber int,
+	@Block nvarchar,
+	@ApartmentNumber int,
+	@PostIndex int
+AS
+INSERT INTO [dbo].[Locations] (
+		[dbo].[Locations].[CityID],
+		[dbo].[Locations].[Street],
+		[dbo].[Locations].[HouseNumber],
+		[dbo].[Locations].[Block],
+		[dbo].[Locations].[ApartmentNumber],
+		[dbo].[Locations].[PostIndex]
+		)
+	VALUES (
+		@CityID,
+		@Street,
+		@HouseNumber,
+		@Block,
+		@ApartmentNumber,
+		@PostIndex
+		)
---
+++ HR_Application_DB_Logic/Repositories/LocationRepository.cs
@@ -0,0 +1,155 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using HR_Application_DB_Logic.Models;
+using Dapper;
+
+namespace HR_Application_DB_Logic.Repositories
+{
+    class LocationRepository
+    {
+        private string _connectionString;
+
+        public LocationRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<LocationDTO> GetAll()
+        {
+            string query = "GetLocations";
+            List<LocationDTO> result = new List<LocationDTO>();
+
+            try
+            {
+                using (IDbConnection dbConnection = new SqlConnection(_connectionString))
+                {
+                    result = dbConnection.Query<LocationDTO>(query).AsList<LocationDTO>();
+                }
+            }
+            catch
+            {
+                result = null;
+            }
+
+            return result;
+        }
+
+        public bool Create(LocationDTO Location)
+        {
+            string query = "CreateLocation @CityID @Street @HouseNumber @Block @ApartmentNumber @PostIndex";
+            bool result = true;
+
+            try
+            {
+                using (IDbConnection dbConnection = new SqlConnection(_connectionString))
+                {
+                    dbConnection.Execute(query, new
+                    { 
+                        Location.CityID,
+                        Location.Street,
+                        Location.HourseNumber,
+                        Location.Block,
+                        Location.ApartmentNumber,
+                        Location.PostIndex
+                    });
+                }
+            }
+            catch
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public bool Update(LocationDTO Location)
+        {
+            string query = "UpdateLocation @ID @CityID @Street @HouseNumber @Block @ApartmentNumber @PostIndex";
+            bool result = true;
+
+            try
+            {
+                using (IDbConnection dbConnection = new SqlConnection(_connectionString))
+                {
+                    dbConnection.Execute(query, new
+                    {
+                        Location.ID,
+                        Location.CityID,
+                        Location.Street,
+                        Location.HourseNumber,
+                        Location.Block,
+                        Location.ApartmentNumber,
+                        Location.PostIndex
+                    });
+                }
+            }
+            catch
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public bool Delete(int id)
+        {
+            string query = "DeleteLocation @ID";
+            bool result = true;
+
+            try
+            {
+                using (IDbConnection dbConnection = new SqlConnection(_connectionString))
+                {
+                    dbConnection.Execute(query, new { id });
+                }
+            }
+            catch
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public LocationDTO GetByID(int id)
+        {
+            string query = "GetLocationsByID @ID";
+            LocationDTO result = new LocationDTO();
+
+            try
+            {
+                using (IDbConnection dbConnection = new SqlConnection(_connectionString))
+                {
+                    result = dbConnection.QuerySingle<LocationDTO>(query, new { id });
+                }
+            }
+            catch
+            {
+                result = null;
+            }
+
+            return result;
+        }
+
+        public LocationDTO GetByStreet(string Name)
+        {
+            string query = "GetLocationsByStreet @Street";
+            LocationDTO result = new LocationDTO();
+
+            try
+            {
+                using (IDbConnection dbConnection = new SqlConnection(_connectionString))
+                {
+                    result = dbConnection.QuerySingle<LocationDTO>(query, new { Name });
+                }
+            }
+            catch
+            {
+                result = null;
+            }
+
+            return result;
+        }
+    }
+}
