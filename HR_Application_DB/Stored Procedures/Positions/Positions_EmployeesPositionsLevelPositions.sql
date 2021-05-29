255293b7ed42abddeaa3aaccb9b48cd78ad5c5ee
Serhii <avakyanms@gmail.com>
2021-05-29 17:53:15 +0300

    Added SP [Positions_EmployeesPositionsLevelPositions

---
+++ HR_Application_DB/Stored Procedures/Positions/Positions_EmployeesPositionsLevelPositions.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [HRAppDB].[Positions_EmployeesPositionsLevelPositions]
+AS
+SELECT
+pe.ID,
+pe.EmployeeID,
+pe.HiredDate,
+pe.FiredDate,
+pe.IsActual,
+lp.ID as IDLevelsPositions,
+lp.Title as TitleLevelsPositions,
+lp.[Description],
+p.ID as IDPositions,
+p.Title as TitlePositions,
+p.[Description]
+
+FROM [HRAppDB].[Positions_Employees] as pe
+INNER JOIN [HRAppDB].[LevelsPositions] as lp
+ON
+pe.LevelPosition=lp.ID
+
+INNER JOIN [HRAppDB].[Positions] as p
+ON
+pe.PositionID=p.ID
