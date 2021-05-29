b88c3c1f71bcc70af4eb25386643b2b37c1a1592
Serhii <avakyanms@gmail.com>
2021-05-29 17:41:08 +0300

    Added SP LocationCitiesCountries

---
+++ HR_Application_DB/Stored Procedures/Locations/LocationCitiesCountries.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [HRAppDB].[LocationCitiesCountries]
+AS
+SELECT 
+lo.ID,
+lo.Street,
+lo.HouseNumber,
+lo.[Block],
+lo.ApartmentNumber,
+lo.PostIndex,
+ci.ID as IDCities,
+ci.Name,
+co.ID as IDCountries,
+co.Name
+
+FROM [HRAppDB].[Locations] as lo
+
+INNER JOIN [HRAppDB].[Cities] as ci
+ON
+lo.CityID=ci.ID
+
+INNER JOIN [HRAppDB].[Countries] as co
+ON
+ci.CountryID=co.ID
