d7e8cd881761632f4cc5b87e26ba8a014e293a4a
Dmitry <Kravchenkodmo@gmail.com>
2021-05-30 15:04:30 +0300

    Refactoring code

--- HR_Application_DB/Stored Procedures/Custom/Companies/GetCompaniesDepartments.sql
+++ HR_Application_DB/Stored Procedures/Custom/Companies/GetCompaniesDepartments.sql
@@ -1,19 +1,17 @@
 ﻿CREATE PROCEDURE [dbo].[GetCompaniesDepartments]
 AS
 SELECT    c.[ID] AS IDCompany,
-c.[Description] AS CompanyDescription,
 c.[Title] AS CompanyTitle,
-cl.[ID] AS IDCompany_Location,
-cl.[LocationID],
+c.[LocationID],
+c.[Description] AS CompanyDescription,
+c.[IsActual] AS CompanyIsActual,
 cd.[ID] AS IDCompanies_Depatments,
 d.[ID] AS IDDepartment,
 d.[Title] AS DepartmentTitle,
 d.[Description] AS DepartmentDescription
 FROM         HRAppDB.Companies AS c 
-RIGHT JOIN HRAppDB.Companies_Locations AS cl
-	ON  c.ID = cl.CompanyID
 LEFT JOIN   HRAppDB.Companies_Depatments AS cd
 	ON c.ID = cd.CompanyID
-INNER JOIN HRAppDB.Departments as d
+LEFT JOIN HRAppDB.Departments as d
 	ON d.ID = cd.DepartmentID
-	WHERE (c.IsActual = 1) AND (cl.IsActual = 1) AND (cd.IsActual = 1)
+	WHERE (c.IsActual = 1) AND (cd.IsActual = 1)
---
+++ HR_Application_DB_Logic/Models/Custom/CompanyDepartmentsDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR_Application_DB_Logic.Models.Custom
+{
+    public class CompanyDepartmentsDTO
+    {
+        public CompanyDTO Company { get; set; }
+        public List<DepartmentDTO> Departments { get; set; }
+    }
+}
