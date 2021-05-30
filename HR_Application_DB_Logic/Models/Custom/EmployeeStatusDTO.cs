f4ab02cf0b96bffa3763028d1868cad494a66eea
Alexey <nhevgevgev@gmail.com>
2021-05-30 20:28:11 +0300

    Fix error

--- HR_Application_DB_Logic/Models/Custom/EmployeeStatusDTO.cs
+++ HR_Application_DB_Logic/Models/Custom/EmployeeStatusDTO.cs
@@ -4,12 +4,12 @@ namespace HR_Application_DB_Logic.Models.Custom
 {
     public class EmployeeStatusDTO
     {
-        public int ID { get; set; }
+        public int? ID { get; set; }
         public string Photo { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime RegistrationDate { get; set; }
-        public int LocationID { get; set; }
+        public int? LocationID { get; set; }
         public bool IsActual { get; set; }
         public StatusDTO Status { get; set; }
     }
