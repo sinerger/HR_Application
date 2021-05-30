d9d5e7494efdde38e3944688702c8ab9fddbd48b
Ann <annaporik12@gmail.com>
2021-05-30 23:09:07 +0300

    Create AdressRepository

---
+++ HR_Application_DB_Logic/Repositories/AdressRepository.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using HR_Application_DB_Logic.Models;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HR_Application_DB_Logic.Repositories
+{
+    public class AdressRepository
+    {
+        private string _connectionString;
+
+        public AdressRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<AdressDTO> GetAll()
+        {
+            string query = "[HRAppDB].[GetAdress]";
+            List<AdressDTO> result = new List<AdressDTO>();
+
+            try
+            {
+                using (IDbConnection dbConnection = new SqlConnection(_connectionString))
+                {
+                    result = dbConnection.Query<AdressDTO, CityDTO, CountryDTO, AdressDTO>
+                        (query,
+                        (adress, city, country) =>
+                        {
+                            adress.City = city;
+                            adress.Country = country;
+
+                            return adress;
+                        }
+                        , splitOn: "IDLocations, IDCities, IDCountries")
+                        .AsList<AdressDTO>();
+                }
+            }
+            catch
+            {
+                result = null;
+            }
+
+            return result;
+        }
+    }
+}
