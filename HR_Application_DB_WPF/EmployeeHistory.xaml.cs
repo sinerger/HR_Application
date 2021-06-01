b21e75e70b689895234c15665fe3708415f16c4d
Serhii <avakyanms@gmail.com>
2021-06-01 13:47:17 +0300

    Add EmployeeProfileHistory tab EmployeeHistory

---
+++ HR_Application_DB_WPF/EmployeeHistory.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HR_Application_DB_WPF
+{
+    /// <summary>
+    /// Interaction logic for EmployeeHistory.xaml
+    /// </summary>
+    public partial class EmployeeHistory : Window
+    {
+        public EmployeeHistory()
+        {
+            InitializeComponent();
+        }
+    }
+}
