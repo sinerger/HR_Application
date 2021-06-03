a04fda56b2bb32048e779e6f09830f40a920ed30
Rostislav <skochmen@gmail.com>
2021-06-03 21:16:12 +0300

    Create projects BLL and Tests

---
+++ HR_Application_BLL.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace HR_Application_BLL.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
