ee97e8f3d11fe4ad1c4250b43e00d624d444c8a6
Rostislav <skochmen@gmail.com>
2021-06-04 22:35:36 +0300

    Add placeholder

--- HR_Application_DB_WPF/Windows/GeneralWindows/AuthorizationWindow.xaml.cs
+++ HR_Application_DB_WPF/Windows/GeneralWindows/AuthorizationWindow.xaml.cs
@@ -18,6 +18,9 @@ namespace HR_Application_DB_WPF.Windows.GeneralWindows
     /// </summary>
     public partial class AuthorizationWindow : Window
     {
+        private const string _defaultTextLogin = "Login";
+        private const string _defaultTextPassword = "Password";
+
         public AuthorizationWindow()
         {
             InitializeComponent();
@@ -28,7 +31,7 @@ namespace HR_Application_DB_WPF.Windows.GeneralWindows
             // TODO: Проверяем можно ли залогиниться 
             UserController userController = new UserController();
 
-            if(userController.Autorization(TextBox_Login.Text, TextBox_Password.Text))
+            if (userController.Autorization(TextBox_Login.Text, TextBox_Password.Text))
             {
                 HomePageWindow homePageWindow = new HomePageWindow();
                 homePageWindow.Show();
@@ -46,6 +49,32 @@ namespace HR_Application_DB_WPF.Windows.GeneralWindows
             registrationWindow.Show();
 
             this.Close();
+        }
+
+        private void TextBox_Login_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (sender is TextBox)
+            {
+                var textBox = (TextBox)sender;
+
+                //if (textBox.Text == string.Empty)
+                //{
+                //    textBox.Text = _defaultTextLogin;
+                //}
+            }
+        }
+
+        private void TextBox_Login_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //if (sender is TextBox)
+            //{
+            //    var textBox = (TextBox)sender;
+
+            //    if (textBox.Text == _defaultTextLogin )
+            //    {
+            //        textBox.Text = string.Empty;
+            //    }
+            //}
         }
     }
 }
