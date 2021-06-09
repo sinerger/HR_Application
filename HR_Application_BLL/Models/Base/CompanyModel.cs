3221027b06d64a4eb5dfd131beeff6ee553da1be
Rostislav <skochmen@gmail.com>
2021-06-09 15:11:54 +0300

    Debug

--- HR_Application_BLL/Models/Base/CompanyModel.cs
+++ HR_Application_BLL/Models/Base/CompanyModel.cs
@@ -10,7 +10,7 @@
 
         public override string ToString()
         {
-            return $"{Title} d.{Description} i.{IsActual}";
+            return $"Title: {Title} Description: {Description}";
         }
 
         public override bool Equals(object obj)
