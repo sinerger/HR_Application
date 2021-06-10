55500c4e019e9363503160c69fe09891411af207
Ann <annaporik12@gmail.com>
2021-06-10 16:10:33 +0300

    fixes bug

--- HR_Application_BLL/Mappers/EmployeePositionMapper.cs
+++ HR_Application_BLL/Mappers/EmployeePositionMapper.cs
@@ -12,28 +12,20 @@ namespace HR_Application_BLL.Mappers
 {
     class EmployeePositionMapper : BaseMapper
     {
-        public List<EmployeePosition> GetUsersFromModels(List<EmployeePositionModel> employeePositionsModel, List<PositionModel> positions, List<LevelsPositionModel> levelsPositions)
-        {
-            if (employeePositionsModel != null && positions != null && levelsPositions != null)
-            {
-                var emploeePositions = _mapper.Map<List<EmployeePosition>>(employeePositionsModel);
-                foreach (var emploeePosition in emploeePositions)
-                {
-                    var employeePositionModel = employeePositionsModel.First(emploeePositionModel => emploeePositionModel.ID == emploeePosition.ID);
-                    employeePosition.PositionID = positions.First(position => position.ID == employeePositionModel.PositionID);
-                    employeePosition.LevelPositionID = levelsPositions.First(levelPosition => levelPosition.ID == employeePositionModel.LevelPositionID);
-                }
-
-                return emploeePositions;
-            }
-            else if (employeePositionsModel == null)
-            {
-                throw new ArgumentNullException("List users EmployeePosition is null");
-            }
-            else
-            {
-                throw new ArgumentNullException("List positions is null");
-            }
-        }
+        //public List<EmployeePosition> GetUsersFromModels(List<EmployeePositionModel> employeePositionsModel, List<PositionModel> positions, List<LevelsPositionModel> levelsPositions)
+        //{
+        //    if (employeePositionsModel != null && positions != null && levelsPositions != null)
+        //    {               
+        //        return _mapper
+        //    }
+        //    else if (employeePositionsModel == null)
+        //    {
+        //        throw new ArgumentNullException("List users EmployeePosition is null");
+        //    }
+        //    else
+        //    {
+        //        throw new ArgumentNullException("List positions is null");
+        //    }
+        //}
     }
 }
