961bb4123723a5093ca5fdfed5c82fb07c729604
Rostislav <skochmen@gmail.com>
2021-06-10 12:15:47 +0300

    Rebase logic mapper department

---
+++ HR_Application_BLL/Mappers/CompanyMapper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR_Application_BLL.Mappers
+{
+    class CompanyMapper
+    {
+    }
+}
--- HR_Application_BLL/Mappers/DepartmentMapper.cs
+++ HR_Application_BLL/Mappers/DepartmentMapper.cs
@@ -17,18 +17,55 @@ namespace HR_Application_BLL.Mappers
         {
             List<Department> departments = _mapper.Map<List<Department>>(departmentsDTO);
 
-            foreach (Department department in departments)
+            foreach (Department currentDepartment in departments)
             {
-                var projectsID = departmentsProjectsDTO.Select(dp => dp.DepartmentID)
-                    .Where(id => id == department.ID);
-                var projects = projectsDTO.Select(pr => pr)
-                    .Where(pr=> projectsID.Contains(pr.ID));
+                var projectsID = departmentsProjectsDTO.Select(department => department.DepartmentID)
+                    .Where(id => id == currentDepartment.ID);
+                var projects = projectsDTO.Select(project => project)
+                    .Where(project=> projectsID.Contains(project.ID));
 
-                department.Projects = new List<ProjectModel>();
-                department.Projects.AddRange(_mapper.Map<List<ProjectModel>>(projects));
+                currentDepartment.Projects = new List<ProjectModel>();
+                currentDepartment.Projects.AddRange(new ProjectMapper().GetModelsFromDTO((List<ProjectDTO>)projects));
             }
 
             return departments;
+        }
+
+        public Department GetDepartmentFromDTO(DepartmentDTO departmentDTO, List<ProjectDTO> projectsDTO,
+            DepartmentProjectsDTO departmentProjectsDTO)
+        {
+            if (departmentDTO != null && projectsDTO != null && departmentProjectsDTO != null)
+            {
+                Department department = _mapper.Map<Department>(departmentDTO);
+                department.Projects = new List<ProjectModel>();
+
+                var projects = projectsDTO.Select(project => project)
+                    .Where(project => departmentProjectsDTO.ProjectsID.Contains((int)project.ID));
+
+                department.Projects.AddRange(new ProjectMapper().GetModelsFromDTO((List<ProjectDTO>)projects));
+
+                return department;
+            }
+            else if(departmentDTO == null)
+            {
+                throw new ArgumentNullException("Department DTO is null");
+            }
+            else if(departmentProjectsDTO == null)
+            {
+                throw new ArgumentNullException("Department Projects DTO is null");
+            }
+
+            throw new ArgumentNullException("List projects DTO is null");
+        }
+
+        public DepartmentDTO GetDTOFromDepartment(Department department)
+        {
+            if (department != null)
+            {
+                return _mapper.Map<DepartmentDTO>(department);
+            }
+
+            throw new ArgumentNullException("Department is null");
         }
     }
 }
