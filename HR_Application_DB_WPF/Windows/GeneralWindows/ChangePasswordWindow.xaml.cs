363c9a5a44c39da2ca7425919f4f1adf06d50b5c
Alexey <nhevgevgev@gmail.com>
2021-06-14 12:57:46 +0300

    Update ChangePassword start

--- HR_Application_DB_WPF/Windows/GeneralWindows/ChangePasswordWindow.xaml.cs
+++ HR_Application_DB_WPF/Windows/GeneralWindows/ChangePasswordWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HR_Application_BLL;
+using HR_Application_BLL.Models;
+using HR_Application_DB_WPF.Classes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -14,14 +17,74 @@ namespace HR_Application_DB_WPF.Windows.GeneralWindows
 {
     public partial class ChangePasswordWindow : Window
     {
+        private Cache _cache;
+        private User _user;
+
         public ChangePasswordWindow()
         {
             InitializeComponent();
+            _cache = Cache.GetCache();
+            _user = new User();
+        }
+
+        private void ChangePassword()
+        {
+            _user.Password = Cryptography.GetHash(TextBox_NewPassword.Text);
+
+            try
+            {
+                AuthorizationController.RegistrationNewUser(_user);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+
+            _cache.SelectedCompany = null;
         }
 
         private void Button_Save_Click(object sender, RoutedEventArgs e)
         {
             // TODO: Поменять пароль у пользователя
+
+            bool isConfirm = true;
+
+            if (!(_user.Password.Equals(TextBox_CurrentPassword.Text)))
+            {
+                MessageBox.Show("Invalid Current password");
+
+                isConfirm = false;
+            }
+            else if (!(AuthorizationController.IsValidPassword(TextBox_NewPassword.Text))
+                && !(AuthorizationController.IsValidPassword(TextBox_ConfirmPassword.Text)))
+            {
+                MessageBox.Show("Invalid New password");
+
+                isConfirm = false;
+            }
+            else if (TextBox_NewPassword.Text == TextBox_ConfirmPassword.Text)
+            {
+                MessageBox.Show("New password must be equal to Confirm password");
+
+                isConfirm = false;
+            }
+            else if (TextBox_NewPassword.Text.Length < 8 || TextBox_ConfirmPassword.Text.Length < 8)
+            {
+                MessageBox.Show("Password >= 8");
+
+                isConfirm = false;
+            }
+
+            if (isConfirm)
+            {
+                ChangePassword();
+
+                HomePageWindow homePageWindow = new HomePageWindow();
+                homePageWindow.Show();
+
+                this.Close();
+            }
+
             this.Close();
         }
 
